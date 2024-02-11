8647a59ec848acad0b1d0b8d22ee85f996cfb327
Wald14 <lukewald@hotmail.com>
2024-02-11 13:09:21 -0600

    Star Rating Project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css'
 import Accordion from './components/accordion'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
 
 
   return (
     <>
-      {/* Accordion compnent */}
       <Accordion />
-      {/* Random color component */}
       <RandomColor />
+      <StarRating numberOfStars={10}/>
     </>
   )
 }
